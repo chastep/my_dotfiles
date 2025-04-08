9238b4a2ee471108a9993d795430b10d307af6a6
Chase Stephens <chasestephens@92.com>
2025-04-08 15:18:26 -0400

    refactor: cws method

--- functions/cws.sh
+++ functions/cws.sh
@@ -4,11 +4,13 @@
 # Shows a workspace chooser using fzf
 
 cws() {
-  local ws_dir ext
-  ws_dir="$HOME"/dev
-  ext=".code-workspace"
-  find $ws_dir/*$ext -exec basename {} \; | \
-  sed -r "s/$ext//" | \
-  fzf --height 30% --prompt "Select Workspace >" --preview-window 75% --preview "bat $ws_dir/{}.code-workspace | jq -r '.folders[].path' | sort" | \
-  xargs -I {} code $ws_dir/{}$ext
+  local ws_dir="$HOME/workspace/dev"
+  local ext=".code-workspace"
+
+  find "$ws_dir" -name "*$ext" -exec basename {} \; | \
+    sed -r "s/$ext//" | \
+    fzf --height 30% --prompt "Select Workspace > " \
+        --preview-window 75% \
+        --preview "bat \"$ws_dir/{}.code-workspace\" --style=plain | jq -r '.folders[].path // empty' | sort" | \
+    xargs -r -I {} code "$ws_dir/{}$ext"
 }
